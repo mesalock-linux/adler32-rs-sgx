c7791d3f3d4a168c1c012c845236b8f8db02df91
Yu Ding <dingelish@gmail.com>
2019-06-23 20:57:48 -0700

    Add SGX Upgrade to 1.0.9

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,14 @@
 //! the zlib implementation.
 
 #![forbid(unsafe_code)]
+#![cfg_attr(all(feature = "mesalock_sgx",
+                not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
+use std::prelude::v1::*;
 
 #[cfg(test)]
 extern crate rand;
